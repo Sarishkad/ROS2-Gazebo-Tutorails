1e6799e7918c0d153ac3b46d1fe24517710e8c8e
Sarishkad <sk.sarish.kad@gmail.com>
2026-03-31 16:01:50 +0530

    c

--- build/autonomous_tb3/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sarish/robotics_workspaces/ros_ws/src/install/autonomous_tb3'
--- build/drive_mobile_robot/CMakeFiles/drive_mobile_robot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/drive_mobile_robot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/drive_mobile_robot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
